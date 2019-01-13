6a76407c3d2c3ad5662ee9d80b393da6a2a47bf4
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2019-01-13 11:01:31 -0600

    fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react'
 import './App.css'
 import Greeting from './Greeting.gen'
-import Hey from './Hey'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Greeting name="Tom" />
-        <Hey />
       </div>
     )
   }
--- src/Hey.js
+++ src/Hey.js
@@ -1,5 +1,7 @@
 import * as React from 'react'
 
-export default function Hey({ name }) {
-  return name
+export default class Hey extends React.Component {
+  render() {
+    return <p>{this.props.name}</p>
+  }
 }
